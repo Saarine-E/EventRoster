e98bda47fb7c0a87a7d10f0ce72eba93bfa1e8a1
Saarine-E <sienihemmo@gmail.com>
2025-03-19 01:30:05 +0200

    Adjust router paths

--- backend/routers/events.py
+++ backend/routers/events.py
@@ -2,19 +2,19 @@ from fastapi import APIRouter, status
 from ..db.models import Group,EventDb
 from ..db import events_crud
 
-router = APIRouter(prefix="/event")
+router = APIRouter(prefix="/event", tags=["events"])
 
 
-@router.get("/events", response_model=list[EventDb])
+@router.get("s/", response_model=list[EventDb])
 def get_all_events():
     return events_crud.get_all_events()
 
-@router.get("/event", response_model=EventDb)
+@router.get("/", response_model=EventDb)
 def get_event(eventId: int = 0, title: str = "", eventDatetime: str = ""):
     return events_crud.get_event(eventId, title, eventDatetime)
         
 
-@router.post("/event", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def new_event(
     title: str, 
     eventDatetime: str, 
@@ -25,7 +25,7 @@ def new_event(
     return events_crud.new_event(title,eventDatetime,duration,description,groups)
 
 
-@router.patch("/event", status_code=status.HTTP_200_OK)
+@router.patch("/", status_code=status.HTTP_200_OK)
 def update_event(
     eventId: int, 
     title: str = "", 
@@ -37,6 +37,6 @@ def update_event(
     return events_crud.update_event(eventId,title,eventDatetime,duration,description,groups)
 
 
-@router.delete("/event", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
 def delete_event(eventId: int):
     return events_crud.delete_event()
--- backend/routers/users.py
+++ backend/routers/users.py
@@ -2,20 +2,20 @@ from fastapi import APIRouter, status
 from ..db.models import UserDb
 from ..db import users_crud
 
-router = APIRouter(prefix="/user")
+router = APIRouter(prefix="/user", tags=["users"])
 
-@router.get("/users", response_model=list[UserDb])
+@router.get("s/", response_model=list[UserDb])
 def get_all_users():
     return users_crud.get_all_users()
 
-@router.get("/user", response_model=UserDb)
+@router.get("/", response_model=UserDb)
 def get_user(userId: int = 0, username: str = ""):
     return users_crud.get_user(userId, username)
 
-@router.post("/user", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def update_user(userId: int, username: str):
     return users_crud.update_user()
 
-@router.delete("/user", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(userId: int):
     return users_crud.delete_user(userId)
